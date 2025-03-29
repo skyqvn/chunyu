3983f09323d08619d46aa3ae8c2026f659791643
sky <sky_8691@qq.com>
2025-03-29 13:32:09 +0800

    ...

--- commit.bat
+++ commit.bat
@@ -6,9 +6,9 @@ attrib -r .\docs\千维中考-假题分类.pdf
 copy ..\千维中考-假题分类.pdf docs
 attrib +r .\docs\千维中考-假题分类.pdf
 
-attrib -r .\docs\26文集.pdf
-copy ..\26文集.pdf docs
-attrib +r .\docs\26文集.pdf
+attrib -r .\docs\二六文集.pdf
+copy ..\二六文集.pdf docs
+attrib +r .\docs\二六文集.pdf
 
 attrib -r .\docs\春语.docx
 copy ..\春语.docx docs
@@ -18,9 +18,9 @@ attrib -r .\docs\千维中考-假题分类.docx
 copy ..\千维中考-假题分类.docx docs
 attrib +r .\docs\千维中考-假题分类.docx
 
-attrib -r .\docs\26文集.docx
-copy ..\26文集.docx docs
-attrib +r .\docs\26文集.docx
+attrib -r .\docs\二六文集.docx
+copy ..\二六文集.docx docs
+attrib +r .\docs\二六文集.docx
 
 set date=%date:~0,4%/%date:~5,2%/%date:~8,2%
 echo %date% > .\docs\version.txt
